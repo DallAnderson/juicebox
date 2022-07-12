9416883ea6e857d7c5202839caf7461a9b1eabcb
Jaydenlundahl <jaydenlundahl20@icloud.com>
2022-07-12 14:56:12 -0600

    added dropTables, createTables, rebuildDB

--- db/seed.js
+++ db/seed.js
@@ -12,5 +12,39 @@ async function testDB() {
     client.end();
   }
 }
-
 testDB();
+
+async function dropTables() {
+  try {
+    await client.query(`
+  
+      `);
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function createTables() {
+  try {
+    await client.query(`
+  
+      `);
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function rebuildDB() {
+  try {
+    client.connect();
+
+    await dropTables();
+    await createTables();
+  } catch (error) {
+    console.error(error);
+  } finally {
+    client.end();
+  }
+}
+
+rebuildDB();
