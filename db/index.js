cf1cfabcc4d4303452cbad62a41dc43486c5a0e0
Jaydenlundahl <jaydenlundahl20@icloud.com>
2022-07-15 15:12:32 -0600

    Added api folder

--- db/index.js
+++ db/index.js
@@ -1,6 +1,13 @@
 const PORT = 5000;
 const express = require("express");
 const server = express();
+const apiRouter = require("../api");
+server.use("../api", apiRouter);
+
+const morgan = require("morgan");
+server.use(morgan("dev"));
+
+server.use(express.json());
 
 server.listen(PORT, () => {
   console.log("The server is up on port", PORT);
